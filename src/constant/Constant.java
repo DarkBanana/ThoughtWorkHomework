39a99607f426fb055b3d116c939edca6f3383077
s1mple <798342418@qq.com>
2017-10-16 16:43:41 +0800

    ThoughtWorkHomework by Zhangzhen 2017.10.16

--- src/constant/Constant.java
+++ src/constant/Constant.java
@@ -1,9 +1,11 @@
 package constant;
 
 public class Constant {
+    /* 车辆报废时间 */
     public static final int OVERHAUL_WRITE_OFF_TIME = 3 * 365;
     public static final int NOT_OVERHAUL_WRITE_OFF_TIME = 6 * 365;
 
+    /* 保养周期 */
     public static final int OVERHAUL_CYCLE = 3;
     public static final int MORE_THAN_THREE_YEARS_CYCLE = 6;
     public static final int LESS_THAN_THREE_YEARS_CYCLE= 12;
